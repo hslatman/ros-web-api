b568ce4111dc4e92ac732355efb7ae182000c25c
Herman Slatman <hermanslatman@hotmail.com>
2017-10-16 20:01:31 +0200

    Add setup for ROS Web API client

---
+++ src/ROSClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SlatmanROSWebAPI;
+
+
+class ROSClient
+{
+    const VERSION = "0.0.1";
+    const USER_AGENT_SUFFIX = "ros-web-api-php-client/";
+    
+    public function __construct(array $config = []) {
+	
+    }
+}
